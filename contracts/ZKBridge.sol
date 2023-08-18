94e89170d97a1a293dc674f8cd0faae0739b7b5f
ZKContract <qb@polyhedra.network>
2023-08-18 14:14:50 +0800

    update

--- contracts/ZKBridge.sol
+++ contracts/ZKBridge.sol
@@ -18,7 +18,19 @@ contract ZKBridge is Initializable, OwnableUpgradeable, IZKBridgeInner {
     using RLPReader for bytes;
     using BytesLib for bytes;
 
-    event A(uint256 aa);
+    event SetFee(uint16 dstChainId, uint256 fee);
+
+    event ClaimFee(address operator, uint256 amount);
+
+    event SetTrustedRemoteAddress(uint16 chainId, address remoteAddress);
+
+    event SetMptVerifier(uint16 chainId, address mptVerifier);
+
+    event SetBlockUpdater(uint16 chainId, address lockUpdater);
+
+    event SetL2MessageReceive(uint16 chainId, address l2MessageReceive);
+
+    event SetL2MessageSend(address l2MessageSend);
 
     bytes32 public constant MESSAGE_TOPIC = 0xb8abfd5c33667c7440a4fc1153ae39a24833dbe44f7eb19cbe5cd5f2583e4940;
 
@@ -76,19 +88,20 @@ contract ZKBridge is Initializable, OwnableUpgradeable, IZKBridgeInner {
         currentNonce = _useNonce(msg.sender, _dstChainId, _dstAddress);
         _payload = abi.encodePacked(chainId, _dstChainId, msg.sender, _dstAddress, currentNonce, _payload);
         if (isL2) {
+            require(address(l2MessageSend) != address(0), "l2MessageSend is not set");
             l2MessageSend.sendMessage{value : l2MessageSend.getFee()}(chainId, msg.sender, _dstChainId, _dstAddress, currentNonce, _payload);
         }
         emit MessagePublished(msg.sender, _dstChainId, currentNonce, _dstAddress, _payload);
     }
 
-    function sendFromL2(uint16 _srcChainId, uint16 _dstChainId, address _dstAddress, bytes memory _payload) external returns (uint64 currentNonce) {
+    function sendFromL2(uint16 _srcChainId, uint16 _dstChainId, address _dstAddress, bytes calldata _payload) external returns (uint64 currentNonce) {
         require(msg.sender == l2MessageReceives[_srcChainId], "caller is not the l2MessageReceive");
         require(_dstChainId != chainId, "Cannot send to same chain");
         currentNonce = _useNonce(msg.sender, _dstChainId, _dstAddress);
         emit MessagePublished(msg.sender, _dstChainId, currentNonce, _dstAddress, _payload);
     }
 
-    function validateTransactionProof(uint16 _srcChainId, bytes32 _srcBlockHash, uint256 _logIndex, bytes memory _mptProof) external {
+    function validateTransactionProof(uint16 _srcChainId, bytes32 _srcBlockHash, uint256 _logIndex, bytes calldata _mptProof) external {
         IMptVerifier mptVerifier = mptVerifiers[_srcChainId];
         IBlockUpdater blockUpdater = blockUpdaters[_srcChainId];
         require(address(mptVerifier) != address(0), "MptVerifier is not set");
@@ -178,13 +191,13 @@ contract ZKBridge is Initializable, OwnableUpgradeable, IZKBridgeInner {
 
     function _estimateFee(uint16 _dstChainId, bytes memory _payload) internal view returns (uint256 bridgeFee) {
         if (isL2) {
-            require(address(l2MessageSend) != address(0), "Not set l2MessageSend");
+            require(address(l2MessageSend) != address(0), "l2MessageSend is not set");
             bridgeFee = l2MessageSend.getFee();
         }
         bridgeFee += fee[_dstChainId];
     }
 
-    function estimateFee(uint16 _dstChainId, bytes memory _payload) external view returns (uint256 bridgeFee){
+    function estimateFee(uint16 _dstChainId, bytes calldata _payload) external view returns (uint256 bridgeFee){
         bridgeFee = _estimateFee(_dstChainId, _payload);
     }
 
@@ -192,34 +205,40 @@ contract ZKBridge is Initializable, OwnableUpgradeable, IZKBridgeInner {
     // onlyOwner
     function setFee(uint16 _dstChainId, uint256 _fee) public onlyOwner {
         fee[_dstChainId] = _fee;
+        emit SetFee(_dstChainId, _fee);
     }
 
     function setTrustedRemoteAddress(uint16 _remoteChainId, address _remoteAddress) external onlyOwner {
-        require(_remoteAddress != address(0), "Zero address");
         trustedRemoteLookup[_remoteChainId] = _remoteAddress;
+        emit SetTrustedRemoteAddress(_remoteChainId,_remoteAddress);
     }
 
     function setMptVerifier(uint16 _chainId, address _mptVerifier) external onlyOwner {
         require(_mptVerifier != address(0), "Zero address");
         mptVerifiers[_chainId] = IMptVerifier(_mptVerifier);
+        emit SetMptVerifier(_chainId, _mptVerifier);
     }
 
     function setBlockUpdater(uint16 _chainId, address _blockUpdater) external onlyOwner {
         require(_blockUpdater != address(0), "Zero address");
         blockUpdaters[_chainId] = IBlockUpdater(_blockUpdater);
+        emit SetBlockUpdater(_chainId, _blockUpdater);
     }
 
     function setL2MessageReceive(uint16 _chainId, address _l2MessageReceive) external onlyOwner {
         require(_l2MessageReceive != address(0), "Zero address");
         l2MessageReceives[_chainId] = _l2MessageReceive;
+        emit SetL2MessageReceive(_chainId, _l2MessageReceive);
     }
 
     function setL2MessageSend(address _l2MessageSend) external onlyOwner {
         require(_l2MessageSend != address(0), "Zero address");
         l2MessageSend = IL2MessageSend(_l2MessageSend);
+        emit SetL2MessageSend(_l2MessageSend);
     }
 
     function claimFees() external onlyOwner {
+        emit ClaimFee(msg.sender, address(this).balance);
         payable(owner()).transfer(address(this).balance);
     }
 
